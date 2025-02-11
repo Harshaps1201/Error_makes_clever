54196ceb57b89a1ff7bab9702f57a842b1e9f626
Harsha P.S <psharsha1201@gmail.com>
2025-02-11 12:16:12 +0530

    connections-Sets

--- Error_Makes_Clever.py
+++ Error_Makes_Clever.py
@@ -1,6 +1,9 @@
-a=(1,2,3,4,5,6)
-b=list(a)
+a={1,2,3,4,1,True,"emc"}
 print(a)
-print(b)
-b.pop()
-print(b)
+a.add(10)
+print(a)
+a.remove(4)
+print(a)
+#pop can remove different object in each run
+a.pop()
+print(a)
