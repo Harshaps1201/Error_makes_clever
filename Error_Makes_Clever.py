634284358ab4a1592ed1b9a9868f946796bfb5da
Harsha P.S <psharsha1201@gmail.com>
2025-02-11 12:34:43 +0530

    function

--- Error_Makes_Clever.py
+++ Error_Makes_Clever.py
@@ -1,22 +1,3 @@
-a={"name":"harsha",
-   "age":25,
-   "location":"Tvpm",
-   "students":["bala","john"]}
-print(a)
-print(a["name"])
-print(a.keys())
-print(a.values())
-a["age"]=24
-print(a)
-a["color"]="red"
-print(a)
-a.update({"location":"India"})
-print(a)
-a.pop("age")
-print(a)
-del a["name"]
-print(a)
-a.clear()
-print(a)
-del a
-print(a)
+def painter():
+    print("painting")
+painter()
