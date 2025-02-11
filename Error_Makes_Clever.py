9662f53deef1cd346d28e4f92e6c7a47f51a2cd0
Harsha P.S <psharsha1201@gmail.com>
2025-02-11 12:43:28 +0530

    funtion prob for add, sub, mul, div

--- Error_Makes_Clever.py
+++ Error_Makes_Clever.py
@@ -1,3 +1,15 @@
-def painter():
-    print("painting")
-painter()
+def add():
+    print("sum =",a+b)
+def sub():
+    print("difference =",a-b)
+def mul():
+    print("product =",a*b)
+def div():
+    print("division =",a/b)
+
+a=int(input("enter 1st number: "))
+b=int(input("enter 2nd number: "))
+add()
+sub()
+mul()
+div()
