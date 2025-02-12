435de16c1129a77d15df909048833b412c42b8c6
Harsha P.S <psharsha1201@gmail.com>
2025-02-12 12:08:19 +0530

    parameter function or aurgument

--- Error_Makes_Clever.py
+++ Error_Makes_Clever.py
@@ -1,15 +1,4 @@
-def add():
-    print("sum =",a+b)
-def sub():
-    print("difference =",a-b)
-def mul():
-    print("product =",a*b)
-def div():
-    print("division =",a/b)
-
-a=int(input("enter 1st number: "))
-b=int(input("enter 2nd number: "))
-add()
-sub()
-mul()
-div()
+def painter(msg):
+    print("message:",msg)
+    
+painter("paint my house")
