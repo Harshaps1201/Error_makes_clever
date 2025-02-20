786435758908b2573470be3b98ae3fa5a54e28f1
Harsha P.S <psharsha1201@gmail.com>
2025-02-20 11:43:35 +0530

    class qn

--- Error_Makes_Clever.py
+++ Error_Makes_Clever.py
@@ -1,23 +1,17 @@
-class goa:
-    name="blah"
-    drink=""
-    def party(self):
-        print("Lets party")
-    def beach(self):
-        print("Enjoying the beach")
-
-Ramesh= goa()
-Suresh= goa()
-
-Ramesh.party()
-Ramesh.name="Ramesh"
-Suresh.name="Suresh"
-Ramesh.drink="Yes"
-Suresh.drink="No"
-print(Ramesh.name)
-print("drink:",Ramesh.drink)
-print(Suresh.name)
-print("drink:",Suresh.drink)
-
-Ramesh.party()
-Suresh.beach()
+class calculator:
+    def __init__(self,a,b):
+        self.num1=a
+        self.num2=b
+    def add(self):
+        print("sum:",self.num1+self.num2)
+    def sub(self):
+        print("diff:",self.num1-self.num2)
+    def mul(self):
+        print("mul:",self.num1*self.num2)
+    def div(self):
+        print("div",self.num1/self.num2)
+obj=calculator(10,2)
+obj.add()
+obj.sub()
+obj.mul()
+obj.div()
